9a047f64c23f05e4711d973b7795b93cf97604f1
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2022-04-12 10:36:20 +0800

    ec2 summary

---
+++ 05/vpc_security.tf
@@ -0,0 +1,32 @@
+data "aws_vpc" "main" {
+  filter {
+    name   = "tag:Name"
+    values = ["Default"]
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = data.aws_vpc.main.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [data.aws_vpc.main.cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
